4f12bd9d89cb7c8c73cd7632cbe3438f0e06fe4c
gsk-007 <gsingh16742@gmail.com>
2025-03-11 20:05:40 +0530

    feat: add auth and admin middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protect = async (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (err) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+};
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not Authorized as an admin");
+  }
+};
+
+export { protect, admin };
