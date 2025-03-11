b56beed3749ad170552c9763fcf40fce95603796
gsk-007 <gsingh16742@gmail.com>
2025-03-11 10:58:01 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+import app from "./server.js";
+
+app.listen(port, () => {
+  console.log(`hello on http://localhost:${port}`);
+});
